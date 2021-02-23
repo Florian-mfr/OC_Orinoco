8ed14ebe881a0b00c41ae2a734924c4959515fed
Florian Maffre <florian.maffre31@gmail.com>
2021-02-23 21:37:05 +0000

    page accueil done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-async function main(){
+function main(){
+    fetch("http://localhost:3000/api/cameras")
+    .then(response => {
+        response.json().then(products => {
 
-    const response = await fetch("http://localhost:3000/api/cameras")
-        .then(resultat => resultat.json())
-        .then(json => json)
-    
-    getName(response)
-    console.log(response)
+            const $productList = document.querySelector('.container')
+
+            for(let i = 0; i < products.length; i++) {
+
+                const product = products[i]
+
+                const $a = document.createElement('a')
+                $a.href = 'product.html'
+                $productList.appendChild($a)
+
+                const $div = document.createElement('div')
+                $div.className = 'container__bloc';
+                $a.appendChild($div)
+
+                const $name = document.createElement('p')
+                $name.className = 'name'
+                $name.textContent = product.name
+                $div.appendChild($name)
+
+                const $img = document.createElement('img')
+                $img.className = 'imageUrl'
+                $img.src = product.imageUrl
+                $div.appendChild($img)
+
+                const $price = document.createElement('p')
+                $price.className = 'price'
+                $price.textContent = product.price/100 + ' €'
+                $div.appendChild($price)
+
+            }
+            
+            console.log(products)
+        })
+    })
+    .catch(error => {
+        console.log('error: ' + error)
+    })
     }
-
-function getName(data){
-
-    const name = data[0].name
-    const imgurl = data[0].imageUrl
-    const price = data[0].price
-
-    document.querySelector('.name').textContent = name
-    document.getElementById("img").srcset = imgurl
-    document.querySelector("#price").innerHTML = price/100 + ",00€"
-}
-
-
 
 main();
 
